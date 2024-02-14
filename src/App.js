1fca72598ce229785a2b94d8da69a02728fa0f2c
adarsh3699 <adarsh3699@gmail.com>
2024-02-14 13:16:58 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './style/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState({});
+	useEffect(() => {
+		try {
+			async function logMovies() {
+				const response = await fetch('https://randomuser.me/api/?page=1&results=1&seed=abc');
+				const fetchData = await response.json();
+				setData(fetchData?.results?.[0]);
+			}
+			logMovies();
+		} catch (error) {
+			console.error('Error:', error);
+		}
+	}, []);
+	return (
+		<div className="App">
+			<div className="container">
+				<div className="card">
+					<div className="card-inner">
+						<div className="front" style={{ background: 'url(' + data?.picture?.large + ')' }}></div>
+						<div className="back" style={{ background: 'url(' + data?.picture?.large + ')' }}></div>
+					</div>
+				</div>
+				<div className="card-info">
+					<h1>
+						{data?.name?.first} {data?.name?.last}
+					</h1>
+					<p>{data?.gender}</p>
+					<p>{data?.email}</p>
+					<p>{data?.phone}</p>
+					<p>
+						{data?.location?.city}, {data?.location?.state}
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
